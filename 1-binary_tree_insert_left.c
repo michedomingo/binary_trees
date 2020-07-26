b85c31319603d0252bebb6eb51b6c16ead924e8a
michedomingo <michedomingo@gmail.com>
2020-07-25 22:52:37 -0700

    Initial commit

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a node as the left-child of another node
+ * @parent: pointer to parent node to insert the left-child in
+ * @value: input value of new node
+ * Return: pointer to created node
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+
+}
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: pointer to parent node to insert the right-child in
+ * @value: input value of new node
+ * Return: pointer to created node
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+
+}
