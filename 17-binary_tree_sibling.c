e0047afd6bf8f60590a1fac75416bdbc9551e1c7
michedomingo <michedomingo@gmail.com>
2020-07-26 19:39:33 +0000

    Task17 solution

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,11 +8,12 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *temp = node->parent;
+	binary_tree_t *temp = NULL;
 
 	if (!node || !node->parent)
 		return (NULL);
 
+	temp = node->parent;
 	if (temp->left == node)
 		return (temp->right);
 	else
