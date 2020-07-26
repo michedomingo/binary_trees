926ebec0750c21dfd23a735db57c8c077be7ffcb
michedomingo <michedomingo@gmail.com>
2020-07-26 06:25:13 +0000

    Task8 solution

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - goes through binary tree using post-order traversal
  * @tree: pointer to root node of tree to traverse
  * @func: function call for each node,
  * value in the node must be passed as a parameter to this function
@@ -9,5 +9,10 @@
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
+	if (!tree || !func)
+		return;
 
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
