c296ce2f02dc7b409a8dc380718c901fa66c2849
michedomingo <michedomingo@gmail.com>
2020-07-26 20:48:07 +0000

    Task22 solution

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,5 +8,18 @@
 
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
+	if (!tree)
+		return (NULL);
 
+	if (tree->right)
+	{
+		tree->parent = tree->right;
+		tree->right = (tree->parent)->left;
+		if (tree->right)
+			(tree->right)->parent = tree;
+		(tree->parent)->left = tree;
+		(tree->parent)->parent = NULL;
+		return (tree->parent);
+	}
+	return (tree);
 }
