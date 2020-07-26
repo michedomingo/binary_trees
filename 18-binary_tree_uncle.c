f2999dfabcc0d9c0607ac6cf3ab353ff24d0b4ea
michedomingo <michedomingo@gmail.com>
2020-07-26 19:50:40 +0000

    Task18 solution

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include "17-binary_tree_sibling.c"
 
 /**
  * binary_tree_uncle - finds the the uncle of a node
@@ -8,5 +9,7 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-
+	if (!node)
+		return (NULL);
+	return (binary_tree_sibling(node->parent));
 }
