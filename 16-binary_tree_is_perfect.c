15ab245bca3683f2e7fdafaf1d5651ba8902276c
michedomingo <michedomingo@gmail.com>
2020-07-26 19:22:20 +0000

    Task16 solution

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,12 +1,20 @@
 #include "binary_trees.h"
+#include "14-binary_tree_balance.c"
+#include "15-binary_tree_is_full.c"
 
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: pointer to root node of tree to check
- * Return:
+ * Return: 1 if perfect, otherwise 0
  */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
 
+	if (binary_tree_is_full(tree) && binary_tree_balance(tree) == 0)
+		return (1);
+
+	return (0);
 }
