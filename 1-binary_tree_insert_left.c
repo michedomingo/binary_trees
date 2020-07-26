f3d8b62fbdfe6273a37ffebcb1048c3e14d351f5
michedomingo <michedomingo@gmail.com>
2020-07-26 05:48:23 +0000

    Task1 solution

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,5 +9,21 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *newNode;
 
+	if (!parent)
+		return (NULL);
+
+	newNode = binary_tree_node(parent, value);
+	if (!newNode)
+		return (NULL);
+
+	if (parent->left)
+	{
+		parent->left->parent = newNode;
+		newNode->left = parent->left;
+	}
+	parent->left = newNode;
+
+	return (newNode);
 }
